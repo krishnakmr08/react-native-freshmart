b06770d523bd430d5c9bcc69f79b546ad8665cf3
Krishna Kumar <kkbhagat20@gmail.com>
2026-08-23 16:54:50 +0530

    chore: configure React Native development tooling

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,27 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+
+        alias: {
+          '@assets': './src/assets',
+          '@features': './src/features',
+          '@service': './src/service',
+          '@styles': './src/styles',
+          '@navigation': './src/navigation',
+          '@components': './src/components',
+          '@state': './src/state',
+          '@utils': './src/utils',
+        },
+
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+      },
+    ],
+
+    'react-native-reanimated/plugin',
+  ],
 };
