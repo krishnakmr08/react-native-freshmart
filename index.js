c943036744666b151da376f78b8cd732ad792e07
Krishna Kumar <kkbhagat20@gmail.com>
2026-08-23 16:55:18 +0530

    chore: configure FreshMart application entry

--- index.js
+++ index.js
@@ -2,8 +2,27 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
+import { AppRegistry, Text, TextInput } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 
+import {
+  configureReanimatedLogger,
+  ReanimatedLogLevel,
+} from 'react-native-reanimated';
+
+// Reanimated logger configuration
+configureReanimatedLogger({
+  level: ReanimatedLogLevel.warn,
+  strict: false,
+});
+
+// Disable font scaling globally
+Text.defaultProps = Text.defaultProps || {};
+Text.defaultProps.allowFontScaling = false;
+
+TextInput.defaultProps = TextInput.defaultProps || {};
+TextInput.defaultProps.allowFontScaling = false;
+
+// Register root component
 AppRegistry.registerComponent(appName, () => App);
